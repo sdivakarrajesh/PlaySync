ff4ffa1eaeb207eeb0d05b280002cb6a7cc7978d
sdivakarrajesh <30803637+sdivakarrajesh@users.noreply.github.com>
2018-09-08 23:37:22 +0530

    emitting and listening added

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const http = require('http')
+const socketIO = require('socket.io')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+const publicPath = path.join(__dirname, 'public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+app.use(express.static(publicPath));
+server.listen(PORT, () => {
+  console.log(`Listening on ${ PORT }`);
+});
+
+app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var currentTime = 0;
+io.on('connection',function(socket){
+    console.log("New user connected");
+    socket.emit('syncTime',{
+      console.log("syncing time");
+  });
+});
